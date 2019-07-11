5c839e1029b5ed1010003c78aba781d7b11c1f35
ksivam2065 <ksivam65@gmail.com>
2019-07-10 21:39:38 -0400

    commit work laptop

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ksivam65/flask-tutorial/venv"
+set -gx VIRTUAL_ENV "/home/ksivam/projects/Flaskr/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
