9a87830a4a2340f5070095aa16c47f2e7cd0127b
kup sivam <ksivam65@gmail.com>
2019-07-11 21:34:54 -0400

    working flaskr

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path,'flaskr.sqlite'),
-    )
+    )  
 
     
     if test_config is None:
@@ -32,7 +32,6 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
-    return app
 
     from . import blog
     app.register_blueprint(blog.bp)
